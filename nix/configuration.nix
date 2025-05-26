c70ea8c21e0200c9e8c69d3b3d261b45d36db7cc
Niapoll <Kolya21082001@gmail.com>
2025-05-26 14:18:31 +0300

    Enable nix-command experimental feature

--- nix/configuration.nix
+++ nix/configuration.nix
@@ -417,5 +417,9 @@
   # this value at the release version of the first install of this system.
   # Before changing this value read the documentation for this option
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+
+  # Enable experimental NixOS features
+  nix.settings.experimental-features = [ "nix-command" ];
+
   system.stateVersion = "24.11";
 }
